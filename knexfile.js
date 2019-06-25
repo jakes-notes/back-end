7bfa16ebc34515e327fc326130a48c1a98582c53
Jake Thomas <jmaxt12@gmail.com>
2019-06-25 14:34:42 -0600

    Updates for Heroku

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,21 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './data/notes.sqlite3' }, 
+    connection: { 
+      filename: './data/notes.sqlite3' 
+    }, 
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
     useNullAsDefault: true, 
     migrations: {
       directory: './data/migrations',
       tableName: 'dbmigrations',
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { 
+      directory: './database/seeds' 
+    },
   },
 };
